ea9d72371cf6488e940d6e904e35d58d24425352
Ghabry <gabriel+github@mastergk.de>
2017-07-30 14:55:37 +0200

    Fix incorrect restoring of Page permissions.
    Thanks to Cherry for pointing this out.

--- eventtrace.cpp
+++ eventtrace.cpp
@@ -26,7 +26,7 @@ void onMoveCommand() {
 		: "=a" (_eax)
 		: "b" (address), [_eax] "a" (_eax), [_edx] "d" (_edx)
 		: "memory");
-	
+
 	// Save all registers and run own code
 	asm("pusha":);
 	ofs << "M\t" << character->id << "\t" << character->_unknown_44 << std::endl;
@@ -39,21 +39,21 @@ bool onStartup(char *pluginName) {
 	// Patch call at 0x4C51A5 to call onMoveCommand
 	VirtualProtect((DWORD*)0x4C51A5, 4, PAGE_EXECUTE_READWRITE, &old_protect);
 	*(DWORD*)(0x4C51A5) = (DWORD)onMoveCommand - 0x4C51A4 - 5;
-	VirtualProtect((DWORD*)0x4C51A5, 4, PAGE_EXECUTE, &old_protect);
-	
+	VirtualProtect((DWORD*)0x4C51A5, 4, old_protect, NULL);
+
 	std::time_t t = std::time(NULL);
 	char timestr[100];
 	strftime(timestr, 100, "%Y-%m-%d-%H-%M-%S", std::localtime(&t));
-	
+
 	std::stringstream ss;
 	ss << "eventtrace_" << timestr << ".txt";
 	ofs.open(ss.str().c_str());
-	
+
 	strftime(timestr, 100, "%Y-%m-%d %H:%M:%S", std::localtime(&t));
 	ofs << "# Session start at " << timestr << std::endl;
 	ofs << "# E\tevt_id\tpage_id\tline_id" << std::endl;
 	ofs << "# M\tevt_id\tline_id" << std::endl;
-	
+
 	return true;
 }
 
@@ -64,19 +64,19 @@ bool onEventCommand(
 		// Invalid line, no idea why RPG_RT executes this
 		return true;
 	}
-		
+
 	ofs << "E\t" << eventId << "\t" << pageId << "\t" << lineId << std::endl;
-	
+
 	return true;
 }
 
-bool onComment(const char*  text,  
-	const RPG::ParsedCommentData*,  
-	RPG::EventScriptLine*,  
-	RPG::EventScriptData*,  
+bool onComment(const char*  text,
+	const RPG::ParsedCommentData*,
+	RPG::EventScriptLine*,
+	RPG::EventScriptData*,
 	int, int, int, int*) {
 	ofs << "# " << text << std::endl;
-	
+
 	return true;
 }
 
@@ -84,7 +84,7 @@ void onExit() {
 	std::time_t t = std::time(NULL);
 	char timestr[100];
 	strftime(timestr, 100, "%Y-%m-%d %H:%M:%S", std::localtime(&t));
-	
+
 	ofs << "# Session end at " << timestr << std::endl;
 	ofs.close();
 }
